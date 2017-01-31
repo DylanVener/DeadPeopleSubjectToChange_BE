1155a806311b2b21aac5cd60d4669c890ca51c41
dylanvener <dylanvener1@gmail.com>
2017-01-30 22:15:09 -0500

    added databases

--- backend.py
+++ backend.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, jsonify
 import bleach
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dylan@localhost/test'
+CORS(app)
 db = SQLAlchemy(app)
 
 @app.route('/')
 def main():
     return 'Hello, World!'
 
+@cross_origin()
 @app.route('/cape_search', methods=['GET'])
 def cape_search():
     cape = request.args.get('c_name')
@@ -19,10 +23,13 @@ def cape_search():
 
     return jsonify([x.as_dict() for x in search])
 
+
 class Cape(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    p_name = db.Column(db.String(80))
-    c_name = db.Column(db.String(80))
+    p_name = db.Column(db.String(30))
+    c_name = db.Column(db.String(30))
+    publisher = db.Column(db.String(30), db.ForeignKey('Publisher.name'))
+    universe = db.Column(db.String(30), db.ForeignKey('Universe.name'))
 
     def __repr__(self):
         return '<User %r>' % self.c_name
@@ -33,6 +40,31 @@ class Cape(db.Model):
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+class KilledBy(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    killee = db.column(db.String(30), db.ForeignKey('Cape.id'))
+    killer = db.column(db.String(30), db.ForeignKey('Cape.id'))
+    description = db.Column(db.String(300))
+    IssueNum = db.Column(db.Integer(30))
+
+
+class Publisher(db.Model):
+    name = db.Column(db.String(30), primary_key = True)
+    h_name = db.Column(db.String(300))
+
+    def __repr__(self):
+        return '<Publisher %s>' % self.name
+
+    def __init__(self, name, h_name):
+        self.name = name
+        self.h_name = h_name
+
+class Universe(db.Model):
+    name = db.Column(db.String(30))
+    size = db.Column(db.String(30))
+    publisher = db.Column(db.String(30), db.ForeignKey('Publisher.name')
+
 
 
 
