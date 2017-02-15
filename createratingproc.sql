8beed9cc762e54ed2dd0c5e68f6465aadec0377e
mphiliposian <philipmj@rose-hulman.edu>
2017-02-14 21:57:08 -0500

    rating create proc
    it makes a rating

---
+++ createratingproc.sql
@@ -0,0 +1,11 @@
+DELIMITER //
+CREATE PROCEDURE `createratingproc` (
+	IN Uname_1 varchar(30), 
+	KID_1 mediumint,
+	Value_1 int
+)
+BEGIN
+	INSERT INTO Ratings(Username, KID, Value)
+	VALUES(Uname_1, KID_1, Value_1);
+END //
+
