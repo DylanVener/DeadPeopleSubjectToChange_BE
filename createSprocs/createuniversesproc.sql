d2c73824d8a1fcbee894b3049f4b06143cc498b1
bubulkr <bubulkr@rose-hulman.edu>
2017-02-15 23:11:44 -0500

    added images

--- createSprocs/createuniversesproc.sql
+++ createSprocs/createuniversesproc.sql
@@ -5,9 +5,10 @@ DROP PROCEDURE IF EXISTS `UniverseCreate`//
 CREATE PROCEDURE `UniverseCreate` (
 	IN U_name_1 varchar(30), 
 	IN Size_1 varchar(30),
-	IN P_name_1 varchar(30)
+	IN P_name_1 varchar(30),
+    IN url varchar(255)
 )
 BEGIN
-	INSERT INTO Universes(U_name,Size,P_name)
-	VALUES(U_name_1, Size_1, P_name_1);
+	INSERT INTO Universes(U_name,Size,P_name,Img_URL)
+	VALUES(U_name_1, Size_1, P_name_1,url);
 END //
--- updateSprocs/updateseriessproc.sql
+++ updateSprocs/updateseriessproc.sql
@@ -1,16 +1,17 @@
 DELIMITER //
 Use test //
 
-DROP PROCEDURE IF EXISTS `SeriesDelete`//
+DROP PROCEDURE IF EXISTS `SeriesUpdate`//
 
-CREATE PROCEDURE `SeriesDelete` (
+CREATE PROCEDURE `SeriesUpdate` (
 	IN S_name_old VARCHAR(30),
 	IN S_name_new VARCHAR(30),
 	IN U_name_new VARCHAR(30),
-	IN P_name_new VARCHAR(30)
+	IN P_name_new VARCHAR(30), 
+    IN url varchar(255)
 )
 BEGIN
 	UPDATE Series
-	SET S_name = S_name_new, U_name = U_name_new, P_name = P_name_new
+	SET S_name = S_name_new, U_name = U_name_new, P_name = P_name_new,Img_URL=url
 	WHERE S_name = S_name_old;
 END //
