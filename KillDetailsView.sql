a748d695fd3c1a605823eb04e9b30024bc9e77ad
bubulkr <bubulkr@rose-hulman.edu>
2017-02-15 23:34:05 -0500

    killdetails view

---
+++ KillDetailsView.sql
@@ -0,0 +1,17 @@
+DELIMITER //
+Use test //
+CREATE VIEW KillDetails AS
+    SELECT 
+        k.KID AS Kill_ID,
+        k.KillerID AS KillerID,
+        k.Killed_ID AS Killed_ID,
+        c1.C_name AS Killed_Name,
+        c2.C_name AS Killer_Name,
+        AVG(r.`Value`) AS Rating
+    FROM
+        ((Kills AS k
+        JOIN Characters AS c1 ON k.KilledID = c1.ID)
+        JOIN Characters AS c2 ON k.KillerID = c2.ID)
+            JOIN
+        Ratings AS r ON k.ID = r.KID
+    GROUP BY k.KID;
