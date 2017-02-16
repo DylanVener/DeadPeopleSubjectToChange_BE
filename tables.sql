0b50207447df7a5b8eb74515f43485ec398c2278
Dylan Vener <dylanvener1@gmail.com>
2017-02-15 23:07:56 -0500

    added image

--- tables.sql
+++ tables.sql
@@ -1,26 +1,11 @@
 USE test
 
-DROP TABLE IF EXISTS Characters;
-CREATE TABLE Characters (
-    ID MEDIUMINT NOT NULL AUTO_INCREMENT,
-    P_name VARCHAR(30),
-    C_name VARCHAR(30),
-    PRIMARY KEY(ID)
-);
-
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users (
-    Username VARCHAR(30) NOT NULL,
-    Password CHAR(32) NOT NULL,
-    Role VARCHAR(30) NOT NULL,
-    PRIMARY KEY(Username)
-);
-
 DROP TABLE IF EXISTS Kills;
 CREATE TABLE Kills (
     ID MEDIUMINT NOT NULL AUTO_INCREMENT,
     KillerID MEDIUMINT,
     KilledID MediumInt,
+    Img_URL VARCHAR(255),
     FOREIGN KEY (KillerID)
     REFERENCES Characters(ID),
     FOREIGN KEY (KilledID)
@@ -40,18 +25,12 @@ CREATE TABLE Ratings (
     CONSTRAINT unID PRIMARY KEY(Username, KID)
 );
 
-DROP TABLE IF EXISTS Publishers;
-CREATE TABLE Publishers (
-    P_name VARCHAR(30),
-    H_name VARCHAR(30),
-    PRIMARY KEY(P_name)
-);
-
 DROP TABLE IF EXISTS Universes;
 CREATE TABLE Universes (
     U_name VARCHAR(30),
     Size VARCHAR(30),
     P_name VARCHAR(30),
+    Img_URL VARCHAR(255),
     FOREIGN KEY (P_name)
     REFERENCES Publishers(P_name),
     PRIMARY KEY (U_name)
@@ -62,6 +41,7 @@ CREATE TABLE Series (
     S_name VARCHAR(30),
     U_name VARCHAR(30),
     P_name VARCHAR(30),
+    Img_URL VARCHAR(255),
     FOREIGN KEY (U_name)
     REFERENCES Universes(U_name),
     FOREIGN KEY (P_name)
@@ -80,5 +60,28 @@ CREATE TABLE CharacterSeries (
     CONSTRAINT sc_ID PRIMARY KEY (S_name, C_ID)
 );
 
+DROP TABLE IF EXISTS Publishers;
+CREATE TABLE Publishers (
+    P_name VARCHAR(30),
+    H_name VARCHAR(30),
+    Img_URL VARCHAR(255),
+    PRIMARY KEY(P_name)
+);
 
+DROP TABLE IF EXISTS Characters;
+CREATE TABLE Characters (
+    ID MEDIUMINT NOT NULL AUTO_INCREMENT,
+    P_name VARCHAR(30),
+    C_name VARCHAR(30) NOT NULL,
+    Img_URL VARCHAR(255),
+    PRIMARY KEY(ID)
+);
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
+    Username VARCHAR(30) NOT NULL,
+    Password CHAR(32) NOT NULL,
+    Role VARCHAR(30) NOT NULL,
+    PRIMARY KEY(Username)
+);
 
