7977c243dc611283f5c39335f1ec933aaa32d851
bubulkr <bubulkr@rose-hulman.edu>
2017-02-19 22:42:07 -0500

    limit 1

--- updateSprocs/updateCharacterSproc.sql
+++ updateSprocs/updateCharacterSproc.sql
@@ -7,9 +7,9 @@ CREATE Procedure `CharacterUpdate` (IN id mediumint,
  	IN pname varchar(30), IN cname varchar(30), IN url varchar(255)
 )
 BEGIN
-	DECLARE p_name varchar(30) DEFAULT (SELECT P_name FROM `Characters` WHERE ID = id);
-	DECLARE c_name varchar(30) DEFAULT (SELECT C_name FROM `Characters` WHERE ID = id);
-    DECLARE i_url varchar(30) DEFAULT (SELECT Img_URL FROM `Characters` WHERE ID = id);
+	DECLARE p_name varchar(30) DEFAULT (SELECT P_name FROM `Characters` WHERE ID = id LIMIT 1);
+	DECLARE c_name varchar(30) DEFAULT (SELECT C_name FROM `Characters` WHERE ID = id LIMIT 1);
+    DECLARE i_url varchar(30) DEFAULT (SELECT Img_URL FROM `Characters` WHERE ID = id LIMIT 1);
     
     IF pname <> '' THEN Set p_name = pname; END IF;
     IF (cname <> '')THEN Set c_name = cname; END IF;
