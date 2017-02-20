98f6f2c5fbf76721cd811ed5f9157b118ef287eb
bubulkr <bubulkr@rose-hulman.edu>
2017-02-19 21:58:40 -0500

    fkjaslfes

--- updateSprocs/updatekillsproc.sql
+++ updateSprocs/updatekillsproc.sql
@@ -7,12 +7,8 @@ CREATE Procedure `KillUpdate`(
  	IN killid mediumint, IN Killer mediumint, IN Killed mediumint, IN diedinissue varchar(50), IN description varchar(255)
 )
 BEGIN
-	IF (SELECT COUNT(*) FROM `Kills` WHERE ID = id) = 0
-    THEN
-		SELECT '404';
-    ELSE IF (SELECT COUNT(*) FROM `Characters` WHERE ID = Killer OR ID = Killed) <> 2
-    THEN
-		SELECT '404';
+	IF ((SELECT COUNT(*) FROM `Kills` WHERE ID = id) = 0)THEN SELECT '404';
+    ELSE IF ((SELECT COUNT(*) FROM `Characters` WHERE ID = Killer OR ID = Killed) <> 2 )THEN SELECT '404';
     Else 
 		UPDATE Kills
 		SET 
