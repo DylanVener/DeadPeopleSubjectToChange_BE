64c03bdf44b22d74d38bc10360081ce22bda84d5
bubulkr <bubulkr@rose-hulman.edu>
2017-02-19 21:15:37 -0500

    character update fix?

--- updateSprocs/updateCharacterSproc.sql
+++ updateSprocs/updateCharacterSproc.sql
@@ -7,22 +7,22 @@ CREATE Procedure `CharacterUpdate` (IN id mediumint,
  	IN pname varchar(30), IN cname varchar(30), IN url varchar(255)
 )
 BEGIN
-	DECLARE p_name varchar(30) DEFAULT (SELECT P_name FROM `Characters` WHERE ID = id)//
-	DECLARE c_name varchar(30) DEFAULT (SELECT C_name FROM `Characters` WHERE ID = id)//
-    DECLARE i_url varchar(30) DEFAULT (SELECT Img_URL FROM `Characters` WHERE ID = id)//
+	DECLARE p_name varchar(30) DEFAULT (SELECT P_name FROM `Characters` WHERE ID = id);
+	DECLARE c_name varchar(30) DEFAULT (SELECT C_name FROM `Characters` WHERE ID = id);
+    DECLARE i_url varchar(30) DEFAULT (SELECT Img_URL FROM `Characters` WHERE ID = id);
     
     IF pname <> '' 
     THEN BEGIN
-		Set p_name = pname//
-	END IF//
+		Set p_name = pname;
+	END IF;
     
-    IF cname <> ''
+    IF (cname <> '')
     THEN BEGIN
-		Set c_name = cname//
-    END IF//
+		Set c_name = cname;
+    END IF;
 	IF url <> ''
     THEN BEGIN
-    SET i_url =  url//
+    SET i_url =  url;
     END IF;
 UPDATE `Characters` 
 SET 
@@ -30,5 +30,5 @@ SET
     C_name = c_name,
     Img_URL = i_url
 WHERE
-    ID = id//
+    ID = id;
 END//
