7f392cef4de388f06912643ca56df221573a61e5
Dylan Vener <dylanvener1@gmail.com>
2017-02-19 22:57:31 -0500

    SQL is dumb

--- updateSprocs/updateCharacterSproc.sql
+++ updateSprocs/updateCharacterSproc.sql
@@ -3,22 +3,15 @@ USE test //
 
 DROP Procedure IF EXISTS `CharacterUpdate`//
 
-CREATE Procedure `CharacterUpdate` (IN id mediumint,
+CREATE Procedure `CharacterUpdate` (IN inId mediumint,
  	IN pname varchar(30), IN cname varchar(30), IN url varchar(255)
 )
 BEGIN
-	DECLARE p_name varchar(30) DEFAULT (SELECT P_name FROM `Characters` WHERE ID = id LIMIT 1);
-	DECLARE c_name varchar(30) DEFAULT (SELECT C_name FROM `Characters` WHERE ID = id LIMIT 1);
-    DECLARE i_url varchar(30) DEFAULT (SELECT Img_URL FROM `Characters` WHERE ID = id LIMIT 1);
-    
-    IF pname <> '' THEN Set p_name = pname; END IF;
-    IF (cname <> '')THEN Set c_name = cname; END IF;
-	IF url <> ''THEN SET i_url =  url; END IF;
 UPDATE Characters 
 SET 
-    P_name = p_name,
-    C_name = c_name,
-    Img_URL = i_url
+    P_name = pname,
+    C_name = cname,
+    Img_URL = url
 WHERE
-    ID = id;
+    ID = inId;
 END //
